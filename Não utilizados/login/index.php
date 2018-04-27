2e49af3208c8a453e793db2aa78507b3eb9f88d0
Jessica <jessica.viana111700@gmail.com>
2018-04-27 15:46:38 -0300

    Add files via upload

---
+++ Não utilizados/login/index.php
@@ -0,0 +1,60 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['usuario_online']) OR !($_SESSION['usuario_online'] == true) ){
+        header('Location: login.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+	<title>Document</title>
+</head>
+<body>
+
+	<div class="social">
+
+		<a href="" class="sair">sair</a>
+
+		<img src="https://avatars1.githubusercontent.com/u/1314923?v=3&s=460" alt="" width="200" height="200">
+		<h3>Bem vindo!</h3>
+	</div>
+<div class="ui middle aligned center aligned grid">
+  <div class="column">
+    <h2 class="ui teal image header">
+      <img src="assets/images/logo.png" class="image">
+      <div class="content">
+        Log-in to your account
+      </div>
+    </h2>
+    <form class="ui large form">
+      <div class="ui stacked segment">
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="user icon"></i>
+            <input type="text" name="email" placeholder="E-mail address">
+          </div>
+        </div>
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="lock icon"></i>
+            <input type="password" name="password" placeholder="Password">
+          </div>
+        </div>
+        <div class="ui fluid large teal submit button">Login</div>
+      </div>
+
+      <div class="ui error message"></div>
+
+    </form>
+
+    <div class="ui message">
+      New to us? <a href="#">Sign Up</a>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
