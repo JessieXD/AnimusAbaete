3cc815193ab1d35cc0fc8dd84abe93f3ce04f253
Jessica <jessica.viana111700@gmail.com>
2018-11-23 15:57:39 -0200

    Add files via upload

--- app/model/CrudAtividade.php
+++ app/model/CrudAtividade.php
@@ -11,40 +11,40 @@ class CrudAtividade{
         $this->conexao = conexao::getConexao();
     }
 
-    public function salvar(Atividade $ativ){ //$cod_atividade = NULL, $titulo, $data, $hora, $hora, $num_vagas, $id_ong, $cod_categori
-        $sql = "INSERT INTO `atividades` (`descricao`, `titulo`, `data`, `hora`, `nro_vagas`, `cod_atividade`, `ong_idong`, `categoria_cod_categoria`) VALUES ('$ativ->descricao', 'pintura', '2018-11-23', '05:12:00', '5', '12', '4', '3');";
+    public function salvar(Atividade $ativ){ //$cod_atividade = NULL, $titulo, $data, $hora, $hora, $num_vagas, $id_ong, $cod_categoria
+        $sql = "INSERT INTO `atividades` (`descricao`, `titulo`, `data`, `hora`, `nro_vagas`, `cod_atividade`, `ong_idong`, `categoria_cod_categoria`) VALUES ('$ativ->descricao', '$ativ->titulo', '$ativ->data', '$ativ->hora', '$ativ->num_vagas', '$ativ->cod_atividade', '$ativ->id_ong', '$ativ->cod_categoria');";
 
         $this->conexao->exec($sql);
     }
 
-    public function editar(Voluntario $user){
-        $this->conexao->exec("UPDATE usuario SET senha = '$user->senha', email = '$user->email', nome = '$user->nome', bio = '$user->bio', imagem = '$user->imagem', site = '$user->site' WHERE cod_user = $user->cod_user ");
+    public function editar(Atividade $ativ){//`descricao`, `titulo`, `data`, `hora`, `nro_vagas`, `cod_atividade`, `ong_idong`, `categoria_cod_categoria`
+        $this->conexao->exec("UPDATE `atividades` SET `descricao` = '$ativ->decricao', `titulo` = '$ativ->titulo', `data` = '$ativ->data', `hora` = '$ativ->hora', `nro_vagas` = '$ativ->num_vagas' cod_ativ = $ativ->cod_atividade ");
     }
 
-    public function getVoluntario( $cod_user){
-        $consulta = $this->conexao->query("SELECT * FROM usuario WHERE cod_user = $cod_user");
-        $user = $consulta->fetch(PDO::FETCH_ASSOC);
+    public function getAtividade( $cod_ativ){
+        $consulta = $this->conexao->query("SELECT * FROM usuario WHERE cod_ativ = $cod_ativ");
+        $ativ = $consulta->fetch(PDO::FETCH_ASSOC);
 
-        return new Voluntario($user['cod_user'], $user['senha'], $user['email'], $user['nome'], $user['user'],  $user['sexo'], $user['idade'],$user['bio'], $user['imagem'], $user['site'], $user['tipo_usuario_idtipo_usuario'] );
+        return new Voluntario($ativ['cod_ativ'], $ativ['senha'], $ativ['email'], $ativ['nome'], $ativ['ativ'],  $ativ['sexo'], $ativ['idade'],$ativ['bio'], $ativ['imagem'], $ativ['site'], $ativ['tipo_usuario_idtipo_usuario'] );
 
     }
 
-    public function getVoluntarios(){
+    public function getAtividades(){
         $consulta = $this->conexao->query("SELECT * FROM usuario");
         $usuarios = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
         $listaUsuarios = [];
 
-        foreach ($usuarios as $user) {
-            $listaUsuarios [] = new Voluntario($user['cod_user'], $user['senha'], $user['email'], $user['nome'], $user['user'],  $user['sexo'], $user['idade'],$user['bio'], $user['imagem'], $user['site']);
+        foreach ($usuarios as $ativ) {
+            $listaUsuarios [] = new Voluntario($ativ['cod_ativ'], $ativ['senha'], $ativ['email'], $ativ['nome'], $ativ['ativ'],  $ativ['sexo'], $ativ['idade'],$ativ['bio'], $ativ['imagem'], $ativ['site']);
         }
 
         return $listaUsuarios;
     }
-    public function excluirVol(int $codigo)
+    public function excluirAtiv(int $codigo)
     {
         //DELETE FROM table_name WHERE condition;
-        $this->conexao->query("DELETE FROM usuario WHERE cod_user = $codigo");
+        $this->conexao->query("DELETE FROM usuario WHERE cod_ativ = $codigo");
     }
 
 }
